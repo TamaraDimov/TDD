9921cde7edf9db28f6c6901c13fac6c5d564ce85
TamaraDimov <tamemojsoska@gmail.com>
2023-08-03 12:42:15 +0200

    update test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,11 +1,13 @@
 require 'rspec'
 require_relative '../solver'
 
-describe Solver do
+RSpec.describe Solver do
   let(:number) { 4 }
   let(:solver) { Solver.new }
 
-  it 'multiplies the given number by all the previous numbers until 1' do
-    expect(solver.solve(number)).to eq(number * (number - 1) * (number - 2) * (number - 3))
-  end
+  describe '#factorial'  do
+    it 'calculates the factorial of the given number' do
+      expect(solver.factorial(number)).to eq(number * (number - 1) * (number - 2) * (number - 3))
+      expect(solver.factorial(0)).to eq(1)
+    end
 end

cd388140aa3908ebe2ff467d1e8cab043d96e663
TamaraDimov <tamemojsoska@gmail.com>
2023-08-03 13:06:04 +0200

    update test

--- solver.rb
+++ solver.rb
@@ -1,2 +1,10 @@
 class Solver
+
+  def factorial(number)
+ 
+  end
+    
+
+    
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Solver do
 
   describe '#factorial'  do
     it 'calculates the factorial of the given number' do
-      expect(solver.factorial(number)).to eq(number * (number - 1) * (number - 2) * (number - 3))
+      expect(solver.factorial(4)).to eq(24)
       expect(solver.factorial(0)).to eq(1)
     end
 end

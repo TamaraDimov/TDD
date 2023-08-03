a7dc602e2b443cac7a586b1983e01f59c998b067
TamaraDimov <tamemojsoska@gmail.com>
2023-08-03 13:54:29 +0200

    edit test for factorial

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe Solver do
     it 'calculates the factorial of the given number' do
       expect(solver.factorial(4)).to eq(24)
       expect(solver.factorial(0)).to eq(1)
+      expect(solver.factorial(-1)).to eq("Invalid number")
     end
   end
 

9548901c2c260b73de6965b6cfb8351b56a926bd
TamaraDimov <tamemojsoska@gmail.com>
2023-08-03 14:13:55 +0200

    update test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe Solver do
 
   describe '#factorial' do
     it 'calculates the factorial of the given number' do
+      expect { solver.factorial(-1) }.to raise_error(StandardError, "Invalid number")
       expect(solver.factorial(4)).to eq(24)
       expect(solver.factorial(0)).to eq(1)
-      expect(solver.factorial(-1)).to eq("Invalid number")
     end
   end
 
